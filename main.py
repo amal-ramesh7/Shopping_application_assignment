2db6484e4397b924116e8e57afbc58e370f2a975
AIPLBLRNT006\Padmapriya V <padmapriya.v@anorasolutions.com>
2025-04-16 11:34:40 +0530

    updated main.py

--- main.py
+++ main.py
@@ -1,65 +1,93 @@
+from datetime import datetime
+import csv
+import pandas as pd
+import re
 
-def sortDates(orderList):
-    split = orderList.split(",")
-    date= split[0].split(" ")[0].split("-")
-    time = split[0].split(" ")[1].split(":")
-    dT = "".join(date) + "".join(time)
-    #print(dT)
-    return (dT)
-#Comment
-
-def recentOrder():
-    orderList = []
-    with open("Orders.txt", "r") as Orders:
-        for line in Orders:
-            orderList.append(line.strip("\n"))
-    #print(orderList)
-    orderList = sorted(orderList, key=sortDates)
-    #print(orderList)
-    print("________________________________________")
-    print(orderList[0])
-    print("________________________________________")
-
-def ordersByDate():
-    orderList = []
-    date = input("Enter the date in the format (YYYY-MM-DD): ")
-    with open("Orders.txt", "r") as Orders:
-        for line in Orders:
-            orderList.append(line.strip("\n"))
+orders = []
+def addProduct():
     
-    print("________________________________________")
-    for order in orderList:
-        if date in order:
-            print(order)
-            continue
-    print("________________________________________")
-
-
-def previousOrders():
+    print("----------------------------------------\nEnter \"back\" to move back to place order page\n----------------------------------------")
+    price = 10 
     while True:
-        print(
-"""----------------------------------------
-Enter \"back\" to move back to place order page
-----------------------------------------""")
+        with open("Data.csv", 'r') as file:
+            data = csv.reader(file)
+            headers = next(data)
+            product_name = input("Enter the product name: ")
+            found = False
+            for row in data:
+                items = []
+                for item in row:
+                    if '/' in item:
+                        items.extend(each.strip(" ") for each in item.split("/"))
+                    elif item=="":
+                        continue
+                    else:
+                        items.append(item)
+                if product_name in items:
+                    found = True
+                    with open("Orders.csv", 'a', newline="") as file:
+                        writer = csv.writer(file)
+                        found = True
+                        quantity = int(input(f"How many {product_name} would you like to add?"))
+                        cost = quantity*price 
+                        current_date_time = datetime.now()
+                        foramtted_date = current_date_time.strftime("%B %d %Y, %H:%M:%S.%f")
+                        order = [foramtted_date, product_name, quantity, cost]
+                        #file.write(",".join(map(str,order))+'\n')
+                        writer.writerow(order)
+                        break
+        if product_name=='back':
+            placeOrder()
+        if not found:
+            print(f"The product '{product_name}' is not available.")
         
-        choice =input("""
-1. View most recent order
-2. Check order by date
-Choice: """)
-        if choice == "1":
-            recentOrder()
-            pass
-        elif choice == "2":
-            ordersByDate()
-            pass
-        elif choice == "back":
-            return
+def removeProduct():
+    print("----------------------------------------\nEnter \"back\" to move back to place order page\n----------------------------------------")
+    price = 10
+    while True:
+        item = input("What product would you like to remove from cart?")
+        lines = []
+        with open("Orders.txt", 'r') as f1:
+            for row in f1:
+                order = row.split(",")
+                
+                if item=="back":
+                    placeOrder()
+                elif item!=order[3]:
+                    lines.append(row)
+                else:
+                    print(f"The product {item} removed from the orders")
+        with open("Orders.txt",'w') as f2:
+            f2.writelines(lines)
+
+
+def placeOrder():
+    action = int(input("\n1.Add product\n2.Remove product\n3.Back\nPlease choose the action to be performed:"))
+    if action==1:
+        addProduct()
+    elif action==2:
+        removeProduct()
+    elif action==3:
+        main()
+    else:
+        print("Enter a valid action")
 
 def main():
-    previousOrders()
+    print("1. View Menu\n2. Place Order\n3. View History\n4. Exit")
+    user_choice = int(input("Enter your choice: "))
+    if user_choice==1:
+        viewMenu()
+    elif user_choice==2:
+        placeOrder()
+    elif user_choice==3:
+        viewHistory()
+    elif user_choice==4:
+        exit()
+    else:
+        print("Enter a valid choice")
 
 if __name__ == "__main__":
-    print("Hello")
+    print("Welcome to online shopping\n--------------------------\n")
     main()
 
 
